dce0d1cc74500c6c5a2b212046ee8f9c19512a57
Erick Pinzon <65203105+erickpinzon18@users.noreply.github.com>
2024-11-18 01:19:07 -0600

    add graph to home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,29 +3,71 @@ import { useAuth } from "../../auth/authContext";
 import { useNavigate } from "react-router-dom";
 import { getDailyMeals } from "../meals/functions/getMeals";
 import { GetGoals } from "../goals/functions/getGoals";
+import { PieChart } from "@mui/x-charts";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 function Home() {
     const { userData, currentUser } = useAuth();
     const navigate = useNavigate();
-    const [meals, setMeals] = useState([]);
     const [loadingMeals, setLoadingMeals] = useState(true);
-    const [proteinGoals, setProteinGoals] = useState({});
+    const [proteinGoals, setProteinGoals] = useState(0);
+    const [carbGoals, setCarbGoals] = useState(0);
+    const [fatGoals, setFatGoals] = useState(0);
+    const [totalProtein, setTotalProtein] = useState(0);
+    const [totalCarbs, setTotalCarbs] = useState(0);
+    const [totalFats, setTotalFats] = useState(0);
+    const [proteinData, setProteinData] = useState([]);
+    const [carbData, setCarbData] = useState([]);
+    const [fatData, setFatData] = useState([]);
 
     useEffect(() => {
         const fetchMeals = async () => {
             if (currentUser) {
                 const userMeals = await getDailyMeals(currentUser.uid);
-                setMeals(userMeals);
                 setLoadingMeals(false);
+                setTotalProtein(
+                    userMeals?.reduce((acc, meal) => acc + meal?.protein, 0)
+                );
+                setTotalCarbs(
+                    userMeals?.reduce((acc, meal) => acc + meal?.carbs, 0)
+                );
+                setTotalFats(
+                    userMeals?.reduce((acc, meal) => acc + meal?.fats, 0)
+                );
+                setProteinData(
+                    userMeals?.map((meal) => ({
+                        id: meal?.id,
+                        label: meal?.mealName,
+                        value: meal?.protein,
+                    }))
+                );
+                setCarbData(
+                    userMeals?.map((meal) => ({
+                        id: meal?.id,
+                        label: meal?.mealName,
+                        value: meal?.carbs,
+                    }))
+                );
+                setFatData(
+                    userMeals?.map((meal) => ({
+                        id: meal?.id,
+                        label: meal?.mealName,
+                        value: meal?.fats,
+                    }))
+                );
             }
         };
 
         const fetchGoals = async () => {
             if (currentUser) {
                 const userGoals = await GetGoals(currentUser.uid);
-                setProteinGoals(userGoals[0]?.proteinGoal || undefined);
+                setProteinGoals(userGoals[0]?.proteinGoal || 0);
+                setCarbGoals(userGoals[0]?.carbGoal || 0);
+                setFatGoals(userGoals[0]?.fatGoal || 0);
             }
-        }
+        };
 
         fetchMeals();
         fetchGoals();
@@ -39,9 +81,25 @@ function Home() {
         navigate("/addMeal");
     };
 
-    const totalProtein = meals.reduce((acc, meal) => acc + meal.protein, 0);
-    const totalCarbs = meals.reduce((acc, meal) => acc + meal.carbs, 0);
-    const totalFats = meals.reduce((acc, meal) => acc + meal.fats, 0);
+    const pieParams = {
+        height: 200,
+        margin: { right: 5 },
+        slotProps: { legend: { hidden: true } },
+        skipAnimation: true, 
+    };
+
+    const addRemainingData = (data, total, goal) => {
+        const remaining = goal - total;
+        if (remaining > 0) {
+            data.push({
+                id: "remaining",
+                label: "Remaining",
+                value: remaining,
+                color: "#e0e0e0", 
+            });
+        }
+        return data;
+    };
 
     return (
         <div className="min-h-screen bg-gray-100 p-6">
@@ -49,77 +107,127 @@ function Home() {
                 {/* Header */}
                 <header className="mb-6">
                     <h1 className="text-2xl font-semibold text-blue-600">
-                        Welcome, {userData.email}!
+                        Welcome, {userData?.email}!
                     </h1>
                     <p className="text-gray-500">
                         Member since:{" "}
-                        {userData.createdAt.toDate().toLocaleDateString()}
+                        {userData?.createdAt?.toDate().toLocaleDateString()}
                     </p>
                 </header>
 
-                <div className="flex justify-between items-center mb-6">
-                    <div>
-                        {/* Dashboard Resumido */}
-                        <section className="bg-white p-6 rounded-lg shadow-md mb-6 mr-3">
-                            <h2 className="text-xl font-semibold text-gray-700 mb-4">
-                                Daily Summary
-                            </h2>
-                            {loadingMeals ? (
-                                <div>Loading meals...</div>
-                            ) : (
-                                <div className="grid grid-cols-3 gap-4">
+                {/* Dashboard Resumido */}
+                <section className="bg-white p-6 rounded-lg shadow-md mb-6 mr-3 flex-auto">
+                    <h2 className="text-xl font-semibold text-gray-700 mb-4">
+                        Daily Summary
+                    </h2>
+                    {loadingMeals ? (
+                        <div>Loading meals...</div>
+                    ) : (
+                        <Stack
+                            direction="row"
+                            width="100%"
+                            textAlign="center"
+                            spacing={2}
+                        >
+                            <Box flexGrow={1}>
+                                <Typography>
+                                    <span className="text-xl font-semibold text-gray-700 mb-2">
+                                        Proteins
+                                    </span>
+                                    <p className={`text-2xl font-bold ${totalProtein <= proteinGoals ? "text-yellow-500" : "text-green-500" }  mb-3`}>
+                                        {totalProtein}g
+                                    </p>
+                                </Typography>
+                                <PieChart
+                                    series={[
+                                        {
+                                            data: addRemainingData([...proteinData], totalProtein, proteinGoals),
+                                            innerRadius: 30,
+                                            outerRadius: 100,
+                                            paddingAngle: 5,
+                                            cornerRadius: 5,
+                                        },
+                                    ]}
+                                    {...pieParams}
+                                />
+                                <div className="flex justify-center">
                                     <div className="text-center">
-                                        <h3 className="text-lg font-medium text-gray-800">
-                                            Proteínas
+                                        <h3 className="text-lg font-medium text-gray-800 mt-3">
+                                            Daily Goal
                                         </h3>
                                         <p className="text-2xl font-bold text-blue-500">
-                                            {totalProtein}g
-                                        </p>
-                                    </div>
-                                    <div className="text-center">
-                                        <h3 className="text-lg font-medium text-gray-800">
-                                            Carbohydrates
-                                        </h3>
-                                        <p className="text-2xl font-bold text-green-500">
-                                            {totalCarbs}g
-                                        </p>
-                                    </div>
-                                    <div className="text-center">
-                                        <h3 className="text-lg font-medium text-gray-800">
-                                            Fats
-                                        </h3>
-                                        <p className="text-2xl font-bold text-red-500">
-                                            {totalFats}g
+                                            {proteinGoals}g
                                         </p>
                                     </div>
                                 </div>
-                            )}
-                        </section>
-                    </div>
-                    <div>
-                        {/* Dashboard Resumido */}
-                        <section className="bg-white p-6 rounded-lg shadow-md mb-6 ml-3">
-                            <h2 className="text-xl font-semibold text-gray-700 mb-4">
-                                Protein goal
-                            </h2>
-                            {/* <PieChart
-                                series={[
-                                    {
-                                    data: [...],
-                                    innerRadius: 30,
-                                    outerRadius: 100,
-                                    paddingAngle: 5,
-                                    cornerRadius: 5,
-                                    startAngle: -45,
-                                    endAngle: 225,
-                                    cx: 150,
-                                    cy: 150,
-                                    }
-                                ]}
-                            /> */}
-                        </section>  
-                    </div>
-                </div>
+                            </Box>
+                            <Box flexGrow={1}>
+                                <Typography>
+                                    <span className="text-xl font-semibold text-gray-700 mb-2">
+                                        Carbs
+                                    </span>
+                                    <p className={`text-2xl font-bold ${totalCarbs <= carbGoals ? "text-green-500" : "text-yellow-500" }  mb-3`}>
+                                        {totalCarbs}g
+                                    </p>
+                                </Typography>
+                                <PieChart
+                                    series={[
+                                        {
+                                            data: addRemainingData([...carbData], totalCarbs, carbGoals),
+                                            innerRadius: 30,
+                                            outerRadius: 100,
+                                            paddingAngle: 5,
+                                            cornerRadius: 5,
+                                        },
+                                    ]}
+                                    {...pieParams}
+                                />
+                                <div className="flex justify-center">
+                                    <div className="text-center">
+                                        <h3 className="text-lg font-medium text-gray-800 mt-3">
+                                            Daily Goal
+                                        </h3>
+                                        <p className="text-2xl font-bold text-blue-500">
+                                            {carbGoals}g
+                                        </p>
+                                    </div>
+                                </div>
+                            </Box>
+                            <Box flexGrow={1}>
+                                <Typography>
+                                    <span className="text-xl font-semibold text-gray-700 mb-2">
+                                        Fats
+                                    </span>
+                                    <p className={`text-2xl font-bold ${totalFats <= fatGoals ? "text-green-500" : "text-red-500" }  mb-3`}>
+                                        {totalFats}g
+                                    </p>
+                                </Typography>
+                                <PieChart
+                                    series={[
+                                        {
+                                            data: addRemainingData([...fatData], totalFats, fatGoals),
+                                            innerRadius: 30,
+                                            outerRadius: 100,
+                                            paddingAngle: 5,
+                                            cornerRadius: 5,
+                                        },
+                                    ]}
+                                    {...pieParams}
+                                />
+                                <div className="flex justify-center">
+                                    <div className="text-center">
+                                        <h3 className="text-lg font-medium text-gray-800 mt-3">
+                                            Daily Goal
+                                        </h3>
+                                        <p className="text-2xl font-bold text-blue-500">
+                                            {fatGoals}g
+                                        </p>
+                                    </div>
+                                </div>
+                            </Box>
+                        </Stack>
+                    )}
+                </section>
 
                 {/* Botón para agregar comida */}
                 <button
